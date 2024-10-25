549c2cb6bb730ae9ab43bfd4fdeaec5af652a19e
Sofía J. D. <sofiaduran1448@gmail.com>
2024-10-24 21:44:38 -0400

    Add tab functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-// alert('Hi, webpack!');
-
 import './style.css';
 
 import homeModule from './modules/homeModule';
@@ -7,7 +5,41 @@ import aboutModule from './modules/aboutModule';
 import menuModule from './modules/menuModule';
 import bookingModule from './modules/bookingModule';
 
+const divContent = document.querySelector('#content');
+const btnHome = document.querySelector('#btnHome');
+const btnAbout = document.querySelector('#btnAbout');
+const btnMenu = document.querySelector('#btnMenu');
+const btnBooking = document.querySelector('#bookingContainer');
+
 homeModule();
-aboutModule();
-menuModule();
-bookingModule();
+
+btnHome.addEventListener('click', function()
+{
+    cleanContent();
+    homeModule();
+});
+
+btnAbout.addEventListener('click', function()
+{
+    cleanContent();
+    aboutModule();
+});
+
+btnMenu.addEventListener('click', function()
+{
+    cleanContent();
+    menuModule();
+});
+
+btnBooking.addEventListener('click', function()
+{
+    cleanContent();
+    bookingModule();
+});
+
+
+
+function cleanContent()
+{
+    divContent.innerHTML = '';
+}
